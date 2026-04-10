e5355fd1604edee4044cd8fb131daf37ccb28d85
Bobbyprogrammer <babardev167@gmail.com>
2026-04-10 10:06:01 +0500

    done project

--- server/index.js
+++ server/index.js
@@ -83,7 +83,7 @@ app.use("/api/medicines", medicineRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
